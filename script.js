8def23b47708fcb7dbe24e52da0b6b17bd88af25
baltasn <nks.mpaltas@gmail.com>
2022-09-25 23:05:25 +0300

    Stage 4/5: Let there be music
    Modify the event handlers so that when you press the assigned keys, audio objects are created and played instead of messages displayed in the console.

--- script.js
+++ script.js
@@ -4,11 +4,16 @@ document.addEventListener("keypress", handleKeyPress);
 
 function handleKeyPress(e) {
   const keys = ['A', 'S', 'D', 'F', 'G', 'H', 'J'];
-  const keyPressed = e.key;
-  if (keys.includes(keyPressed.toUpperCase())) {
-    console.log(`The '${keyPressed}' key is pressed.`)
-  } else {
-    console.log("WRONG KEY!!!");
+  const keyPressed = e.key.toUpperCase();
+  if (keys.includes(keyPressed)) {
+    createSoundObject(keyPressed).play();
   }
 }
+
+function createSoundObject(keyPressed) {
+  const audio = new Audio(`white_keys/${keyPressed}.mp3`);
+  return audio;
+}
+
+
 
