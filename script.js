51a9db2c735fd03b43173a8883c168607155d29e
baltasn <nks.mpaltas@gmail.com>
2022-09-26 17:18:56 +0300

    visual changes
    Pressed keys effects

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 "use strict"
 
 document.addEventListener("keypress", handleKeyPress);
+document.addEventListener("keyup", handleKeyRelease);
 
 const whiteKeys = ['A', 'S', 'D', 'F', 'G', 'H', 'J'];
 const blackKeys = ['W', 'E', 'T', 'Y', 'U'];
@@ -9,13 +10,27 @@ function handleKeyPress(e) {
   const keyPressed = e.key.toUpperCase();
   if (whiteKeys.includes(keyPressed) || blackKeys.includes(keyPressed)) {
     createSoundObject(keyPressed).play();
+    pressKeyVisual(keyPressed);
   }
 }
-
 function createSoundObject(keyPressed) {
   const audio = new Audio(`keys/${keyPressed}.mp3`);
   return audio;
 }
+function pressKeyVisual(keyPressed) {
+  const keyPressedEl = document.getElementById(`${keyPressed.toLowerCase()}Key`);
+  keyPressedEl.classList.add("kbd--pressed");
+}
+
+function handleKeyRelease(e) {
+  const keyReleased = e.key.toUpperCase();
+  if (whiteKeys.includes(keyReleased) || blackKeys.includes(keyReleased)) {
+    const keyReleasedEl = document.getElementById(`${keyReleased.toLowerCase()}Key`);
+    keyReleasedEl.classList.remove("kbd--pressed");
+  }
+}
+
+
 
 
 
